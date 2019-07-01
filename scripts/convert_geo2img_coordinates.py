26442b054e5e9e850edd5295f9110aaa4c75ab9f
soliveir <sofia.oliveiraares@epfl.ch>
2019-07-01 16:12:11 +0200

    scripts update

--- scripts/convert_geo2img_coordinates.py
+++ scripts/convert_geo2img_coordinates.py
@@ -3,18 +3,16 @@ __author__ = "solivr"
 __license__ = "GPL"
 
 import click
+***REMOVED***
 from typing import List
-
-
 from geojson_processing.geo_info import offset_geojson_file
 
 @click.command
 @click.option('--geotif_directory', help='Path to the directory containing the geotif images')
 @click.option('--output_dir', help='Output directory for converted geojson files')
 @click.argument('filenames', nargs=-1)
-def convert_coords_from_geo_to_img(filenames: List[str], geotif_directory: str, output_dir: str)
-
-    for filename in filenames:
+def convert_coords_from_geo_to_img(filenames: List[str], geotif_directory: str, output_dir: str):
+    for filename in tqdm(filenames):
         offset_geojson_file(filename, geotif_directory, output_dir)
 
 
