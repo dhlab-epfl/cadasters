0cd8bcebf34d0c5d13e9c1d86ea80cb4304c9ee0
soliveir <sofia.oliveiraares@epfl.ch>
2019-07-05 16:10:06 +0200

    formatting

--- geojson_processing/evaluation.py
+++ geojson_processing/evaluation.py
@@ -34,8 +34,8 @@ def transcription_get_precision(dataframe_correspondencies: Union[pd.DataFrame, 
                                 return_number: bool = False):
 
     # Count also the nan prediction when there wasn't any transcription
-    #correct_nan = sum(dataframe_correspondencies[dataframe_correspondencies.manual_ID == -1].best_transcription.isna())
-    #print(correct_nan)
+    # correct_nan = sum(dataframe_correspondencies[dataframe_correspondencies.manual_ID==-1].best_transcription.isna())
+    # print(correct_nan)
     df_has_transcription = dataframe_correspondencies[~dataframe_correspondencies.best_transcription.isna()]
 
     correct_transcript = sum(df_has_transcription.best_transcription == df_has_transcription.manual_ID)
@@ -45,7 +45,7 @@ def transcription_get_precision(dataframe_correspondencies: Union[pd.DataFrame, 
         return correct_transcript / len(df_has_transcription), correct_transcript
     else:
         # return (correct_transcript + correct_nan) / len(df_has_transcription)
-        return correct_transcript/ len(df_has_transcription)
+        return correct_transcript / len(df_has_transcription)
 
 
 def transcription_get_recall(dataframe_correspondencies: Union[pd.DataFrame, gpd.GeoDataFrame],
@@ -53,7 +53,7 @@ def transcription_get_recall(dataframe_correspondencies: Union[pd.DataFrame, gpd
                              return_number: bool = False):
 
     # Count also the nan prediction when there wasn't any transcription
-    # correct_nan = sum(dataframe_correspondencies[dataframe_correspondencies.manual_ID == -1].best_transcription.isna())
+    # correct_nan = sum(dataframe_correspondencies[dataframe_correspondencies.manual_ID==-1].best_transcription.isna())
 
     correct_transcript = sum(dataframe_correspondencies.manual_ID == dataframe_correspondencies.best_transcription)
     gt_has_transcription = dataframe_gt[~dataframe_gt.ID.isna()]
@@ -128,16 +128,21 @@ def transcriptions_without_outliers_get_recall(dataframe_correspondencies: Union
 
 def get_correspondencies(dataframe_shapes_automatically_produced: Union[pd.DataFrame, gpd.GeoDataFrame],
                          dataframe_shapes_mannually_produced: Union[pd.DataFrame, gpd.GeoDataFrame],
-                         n_neighbors:int=3) -> Union[pd.DataFrame, gpd.GeoDataFrame]:
+                         n_neighbors: int=3) -> Union[pd.DataFrame, gpd.GeoDataFrame]:
 ***REMOVED***"
 
 
     :param dataframe_shapes_automatically_produced: must contain columns `uuid`, `best_transcription`, `geometry`,
     `image_filename`, `is_outlier`
-    :param dataframe_shapes_mannually_produced: must_contain columns `ID`, `uuid`, `geometry`, `centroid`
+    :param dataframe_shapes_mannually_produced: must_contain columns `ID`, `uuid`, `geometry`
+    :param n_neighbors: number of neighbors to consider
     :return: a dataframe of the sime size as `dataframe_shapes_automatically_produced` with the corresponding shapes
     of the `dataframe_shapes_mannually_produced`
 ***REMOVED***"
+
+    # compute centroids for manually annotated data
+    dataframe_shapes_mannually_produced['centroid'] = dataframe_shapes_mannually_produced.geometry.apply(lambda s: s.centroid)
+
     #  Need to create correspondencies between manually corrected and extracted
     df_correspondances = dataframe_shapes_automatically_produced.copy()
     columns_to_add = ['iou', 'manual_geom', 'manual_ID', 'manual_uuid']
@@ -168,7 +173,9 @@ def get_correspondencies(dataframe_shapes_automatically_produced: Union[pd.DataF
         # Find 3 closest shapes in the auto processed shapes
         df_nns = df_correspondances.loc[df_correspondances.index[indexes_neighboring_centroids].values[0]]
 
+        # Get the row corresponding to the manual geometry
         row_manual = dataframe_shapes_mannually_produced.iloc[i]
+        # Compute their IOUs
         nearest_ious = df_nns.geometry.apply(
             lambda s: s.intersection(row_manual.geometry).area / s.union(row_manual.geometry).area).sort_values(
             ascending=False)
@@ -184,7 +191,7 @@ def get_correspondencies(dataframe_shapes_automatically_produced: Union[pd.DataF
 
         df_correspondances.loc[index, 'iou'] = iou
         # -1 to indicate there was no ID
-        df_correspondances.loc[index, 'manual_ID'] = int(row_manual.ID) if not np.isnan(row_manual.ID) else -1
+        df_correspondances.loc[index, 'manual_ID'] = int(row_manual.ID) if not pd.isnull(row_manual.ID) else -1
         df_correspondances.loc[index, 'manual_geom'] = row_manual.geometry
 
         if iou > 0:
